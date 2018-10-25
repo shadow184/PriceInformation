3a4a2a1bd5b75dba9991f9eb120a395941685fbd
Jeff Sugget <jeffrey.sugget@gmail.com>
2018-10-25 20:21:17 +0800

    Update dan-murphys spider

--- PriceInformation/spiders/dan_murphys.py
+++ PriceInformation/spiders/dan_murphys.py
@@ -6,7 +6,7 @@ class DanMurphysBeerSpider(scrapy.Spider):
     name = 'dan-murphys-beer'
 
     def start_requests(self):
-        yield scrapy.Request('https://www.danmurphys.com.au/current-offers?filters=variety(beer)', self.parse, meta={
+        yield scrapy.Request('https://www.danmurphys.com.au/current-offers?filters=variety(beer)&size=120', self.parse, meta={
             'splash': {
                 'endpoint': 'render.html',
                 'args': { 'wait': 0.5 }
@@ -15,8 +15,9 @@ class DanMurphysBeerSpider(scrapy.Spider):
 
     def parse(self, response):
         for p in response.css('.product-content'):
+            product = p.css('.title::text').extract_first()
             yield {
-                    'product': p.css('.title::text').extract(),
+                    'product': product,
                     'price': p.css('.value::text').extract(),
                     'quantity': p.css('.quantity::text').extract()
             }
