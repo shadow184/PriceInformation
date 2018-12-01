249457b886265c2d7a88fe3477485ccc9e978d84
Jeff Sugget <jeffrey.sugget@gmail.com>
2018-12-01 19:49:22 +0800

    Converted to crawler

--- PriceInformation/spiders/dan_murphys.py
+++ PriceInformation/spiders/dan_murphys.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from danMurphysParser import danMurphysParser
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
 class DanMurphysBeerSpider(scrapy.Spider):
     name = 'dan-murphys'
@@ -17,3 +19,39 @@ class DanMurphysBeerSpider(scrapy.Spider):
 
     def parse(self, response):
         yield danMurphysParser(response)
+
+class DanMurphysSpiritsSpider(scrapy.Spider):
+	name = 'dan-murphys-spirits'
+	def start_requests(self):
+		url = 'https://www.danmurphys.com.au/current-offers?filters=variety(spirits)&size=120'
+
+		yield scrapy.Request(url, self.parse, meta={
+			'splash': {
+				'endpoint': 'render.html',
+				'args': { 'wait': 0.5}
+			}
+		})
+
+	def parse(self, response):
+		yield danMurphysParser(response)
+
+class DanMurphysRedWineSpider(scrapy.Spider):
+	name = 'dan-murphys-red-wine'
+	def start_requests(self):
+		url = 'https://www.danmurphys.com.au/current-offers?filters=variety(red-wine)&size=120'
+
+		yield scrapy.Request(url, self.parse, meta={
+			'splash': {
+				'endpoint': 'render.html',
+				'args': { 'wait': 0.5}
+			}
+		})
+
+	def parse(self, response):
+		yield danMurphysParser(response)
+
+process = CrawlerProcess(get_project_settings())
+process.crawl(DanMurphysBeerSpider)
+#process.crawl(DanMurphysSpiritsSpider)
+#process.crawl(DanMurphysRedWineSpider)
+process.start()
